4b74e6eed521ab630f6683046c4a1bb00614f51c
iarchii <artur.latoszewski@gmail.com>
2017-09-08 21:00:13 +0200

    rest api provider

--- app/src/main/java/xyz/thecodeside/tradeapp/repository/remote/rest/RestApiProvider.kt
+++ app/src/main/java/xyz/thecodeside/tradeapp/repository/remote/rest/RestApiProvider.kt
@@ -1,8 +1,35 @@
 package xyz.thecodeside.tradeapp.repository.remote.rest
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import xyz.thecodeside.tradeapp.BuildConfig
+import xyz.thecodeside.tradeapp.helpers.BASE_URL
+import java.util.concurrent.TimeUnit
+
 class RestApiProvider {
     fun provide(): RemoteDataSource {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val loggingInterceptor = HttpLoggingInterceptor()
+        loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+
+        val builder = OkHttpClient.Builder()
+        builder.connectTimeout(5, TimeUnit.SECONDS)
+        if (BuildConfig.DEBUG) {
+            builder.addInterceptor(loggingInterceptor)
+        }
+
+        val client = builder.build()
+
+        val retrofit: Retrofit = Retrofit.Builder()
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl(BASE_URL)
+                .client(client)
+                .build()
+
+        return retrofit.create(RemoteDataSource::class.java)
     }
 
 }
