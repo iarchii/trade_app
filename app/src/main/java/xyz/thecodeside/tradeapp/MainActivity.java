a996964e6972f34f145ae1301b116bae6a06e2d5
iarchii <artur.latoszewski@gmail.com>
2017-09-08 18:49:57 +0200

    init

---
+++ app/src/main/java/xyz/thecodeside/tradeapp/MainActivity.java
@@ -0,0 +1,13 @@
+package xyz.thecodeside.tradeapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
