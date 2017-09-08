2d93a9967eb529bfbb8d7f6797ed4faaff7099e0
iarchii <artur.latoszewski@gmail.com>
2017-09-08 20:53:26 +0200

    rest api get

---
+++ app/src/main/java/xyz/thecodeside/tradeapp/model/Product.kt
@@ -0,0 +1,5 @@
+package xyz.thecodeside.tradeapp.model
+
+class Product {
+
+}
--- app/src/main/java/xyz/thecodeside/tradeapp/repository/remote/rest/RemoteDataSource.kt
+++ app/src/main/java/xyz/thecodeside/tradeapp/repository/remote/rest/RemoteDataSource.kt
@@ -1,5 +1,14 @@
 package xyz.thecodeside.tradeapp.repository.remote.rest
 
+import io.reactivex.Single
+import retrofit2.http.GET
+import retrofit2.http.Path
+import xyz.thecodeside.tradeapp.model.Product
+
 interface RemoteDataSource {
 
+    @GET("core/16/products/{productId}")
+    fun getProductDetails(@Path("productId") userId: String): Single<Product>
+
 }
+
