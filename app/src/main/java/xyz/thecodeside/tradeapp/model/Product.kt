0e148a0eaead7578ee22bbe366e3f174e3d6eef3
iarchii <artur.latoszewski@gmail.com>
2017-09-10 21:41:58 +0200

    product model

--- app/src/main/java/xyz/thecodeside/tradeapp/model/Product.kt
+++ app/src/main/java/xyz/thecodeside/tradeapp/model/Product.kt
@@ -1,5 +1,26 @@
 package xyz.thecodeside.tradeapp.model
 
-class Product {
+import android.support.annotation.Keep
 
-}
+@Keep
+data class Product(
+		var symbol: String,// FRANCE40
+		var securityId: String,// 26608
+		var displayName: String,// French Exchange
+		var currentPrice: CurrentPrice,
+		var closingPrice: ClosingPrice
+)
+
+@Keep
+data class CurrentPrice(
+		var currency: String,// EUR
+		var decimals: Int,// 1
+		var amount: String// 4371.8
+)
+
+@Keep
+data class ClosingPrice(
+		var currency: String,// EUR
+		var decimals: Int,// 1
+		var amount: String// 4216.4
+)
