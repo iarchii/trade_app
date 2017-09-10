316b822ca171902d0fb6790501c60fe325ba3c6f
iarchii <artur.latoszewski@gmail.com>
2017-09-10 19:50:45 +0200

    product item, recycler view in activity

--- app/src/main/java/xyz/thecodeside/tradeapp/list/MainListActivity.kt
+++ app/src/main/java/xyz/thecodeside/tradeapp/list/MainListActivity.kt
@@ -4,11 +4,11 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import xyz.thecodeside.tradeapp.R
 
-class MainActivity : AppCompatActivity() {
+class MainListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main_list)
     }
 }
 
