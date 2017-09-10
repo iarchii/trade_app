b5b9036b0266be6cf951ee0fe469495bf9444dbd
iarchii <artur.latoszewski@gmail.com>
2017-09-10 22:51:39 +0200

    styling

--- app/src/main/java/xyz/thecodeside/tradeapp/productdetails/ProductDetailsActivity.kt
+++ app/src/main/java/xyz/thecodeside/tradeapp/productdetails/ProductDetailsActivity.kt
@@ -2,9 +2,10 @@ package xyz.thecodeside.tradeapp.productdetails
 
 import android.content.Context
 import android.content.Intent
+import android.support.v7.app.AppCompatActivity
 import xyz.thecodeside.tradeapp.model.Product
 
-class ProductDetailsActivity {
+class ProductDetailsActivity  : AppCompatActivity(){
     companion object {
         private const val PRODUCT_KEY = "PRODUCT_KEY"
         fun show(context: Context, product: Product){
--- app/src/main/java/xyz/thecodeside/tradeapp/productlist/ProductListActivity.kt
+++ app/src/main/java/xyz/thecodeside/tradeapp/productlist/ProductListActivity.kt
@@ -3,10 +3,10 @@ package xyz.thecodeside.tradeapp.productlist
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.GridLayoutManager
-import xyz.thecodeside.tradeapp.helpers.showToastShort
 import kotlinx.android.synthetic.main.activity_product_list.*
 import xyz.thecodeside.tradeapp.R
 import xyz.thecodeside.tradeapp.TradeApp
+import xyz.thecodeside.tradeapp.helpers.showToastShort
 import xyz.thecodeside.tradeapp.model.Product
 import xyz.thecodeside.tradeapp.productdetails.ProductDetailsActivity
 import javax.inject.Inject
@@ -16,32 +16,34 @@ class ProductListActivity : AppCompatActivity(), ProductListPresenter.ProductLis
     @Inject lateinit var presenter: ProductListPresenter
     var adapter: ProductListAdapter = ProductListAdapter()
 
+    private val PRODUCT_COLUMNS = 2
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_product_list)
         TradeApp.baseComponent.inject(this)
         setupRecycler()
-    }
-
-    override fun onStart() {
         presenter.attachView(this)
-        super.onStart()
     }
 
-    override fun onStop() {
+
+
+    override fun onDestroy() {
         presenter.detachView()
         super.onStop()
     }
-
 
     private fun setupRecycler() {
         adapter.clickListener = object : ProductClickListener {
             override fun onClick(product: Product) {
                 presenter.handleProductClick(product)
             }
-
         }
-        productsRV.layoutManager = GridLayoutManager(this, 2)
+
+        productsRV.layoutManager = GridLayoutManager(this, PRODUCT_COLUMNS)
+        productsRV.addItemDecoration(GridItemDecoration(
+                resources.getDimensionPixelSize(R.dimen.product_spaceing),
+                PRODUCT_COLUMNS))
         productsRV.adapter = adapter
 
     }
