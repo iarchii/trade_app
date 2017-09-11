e4150a3a139097f7131f811e3d03d76c9be30a07
iarchii <artur.latoszewski@gmail.com>
2017-09-11 21:24:23 +0200

    product details v1

--- app/src/main/java/xyz/thecodeside/tradeapp/productdetails/ProductDetailsActivity.kt
+++ app/src/main/java/xyz/thecodeside/tradeapp/productdetails/ProductDetailsActivity.kt
@@ -4,17 +4,20 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import kotlinx.android.synthetic.main.circle_item_view.*
+import kotlinx.android.synthetic.main.activity_product_details.*
 import xyz.thecodeside.tradeapp.R
 import xyz.thecodeside.tradeapp.TradeApp
-import xyz.thecodeside.tradeapp.helpers.NumberFormatter
 import xyz.thecodeside.tradeapp.model.Product
 import javax.inject.Inject
 
-class ProductDetailsActivity  : AppCompatActivity(), ProductDetailsPresenter.ProductDetailsView{
+class ProductDetailsActivity : AppCompatActivity(), ProductDetailsPresenter.ProductDetailsView {
     override fun showProduct(product: Product) {
-        productName.text = product.displayName
-        productPrice.text = NumberFormatter.format(product.currentPrice.amount)
+        displayNameTv.text = product.displayName
+        symbolTv.text = product.symbol
+        idTv.text = getString(R.string.id_format,product.securityId)
+        currentPriceTv.text = product.currentPrice.amount.toString()
+        closingPriceTv.text = product.closingPrice.amount.toString()
+
     }
 
     override fun showError() {
@@ -23,7 +26,7 @@ class ProductDetailsActivity  : AppCompatActivity(), ProductDetailsPresenter.Pro
 
     companion object {
         private const val PRODUCT_KEY = "PRODUCT_KEY"
-        fun show(context: Context, product: Product){
+        fun show(context: Context, product: Product) {
             val intent = Intent(context, ProductDetailsActivity::class.java)
             intent.putExtra(PRODUCT_KEY, product)
             context.startActivity(intent)
@@ -44,7 +47,6 @@ class ProductDetailsActivity  : AppCompatActivity(), ProductDetailsPresenter.Pro
         presenter.detachView()
         super.onDestroy()
     }
-
 
 
 }
