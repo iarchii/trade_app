a6a62694eb1a7aebf1b25dbbef773aecadea11d4
iarchii <artur.latoszewski@gmail.com>
2017-09-13 01:00:33 +0200

    delete old comment

--- app/src/main/java/xyz/thecodeside/tradeapp/helpers/ViewExtensions.kt
+++ app/src/main/java/xyz/thecodeside/tradeapp/helpers/ViewExtensions.kt
@@ -15,15 +15,7 @@ fun View.invisible() {
     this.visibility = View.INVISIBLE
 }
 
-/**
- * This method converts dp unit to equivalent pixels, depending on device density.
 
- * @param dp A value in dp (density independent pixels) unit. Which we need to convert into pixels
- * *
- * @param context Context to get resources and device specific display metrics
- * *
- * @return A value to represent px equivalent to dp depending on device density
- */
 fun View.convertDpToPixel(dp: Int): Int{
     val resources = context.resources
     val metrics = resources.displayMetrics
@@ -31,15 +23,6 @@ fun View.convertDpToPixel(dp: Int): Int{
     return px.toInt()
 }
 
-/**
- * This method converts device specific pixels to density independent pixels.
-
- * @param px A value in px (pixels) unit. Which we need to convert into db
- * *
- * @param context Context to get resources and device specific display metrics
- * *
- * @return A value to represent dp equivalent to px value
- */
 fun View.convertPixelsToDp(px: Int): Int {
     val resources = context.resources
     val metrics = resources.displayMetrics
